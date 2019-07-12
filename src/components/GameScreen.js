23e812be429b65fc451453360b58303c31ef9e60
Cherry Poonolly <rpoonolly@gmail.com>
2019-07-12 13:02:26 -0400

    WIP - Adding a simple description

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -10,6 +10,18 @@ class GameScreen extends React.Component {
       <div className="game-screen">
         <GameHeader game={this.props.game}/>
         <GameGrid game={this.props.game}/>
+
+        <div className="game-screen-rules">
+          <p>
+            <a href="https://en.wikipedia.org/wiki/Minesweeper_(video_game)">How To Play</a><br/>
+            <br/>
+            Controls:<br/>
+            Left-Click select a square<br/>
+            Right-Click flag a square as a mine<br/>
+            <br/>
+            Game is only over when all squares have been either selected or flagged
+          </p>
+        </div>
       </div>
     );
   }
